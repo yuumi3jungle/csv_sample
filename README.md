745c5adbcc8bbbd079a789623c70d188956b1b5b
Yuumi Yoshida <yy@ey-office.com>
2015-07-24 11:03:09 +0900

    Scaffold and seed data

---
+++ README.md
@@ -0,0 +1,13 @@
+# CSV Upload/Download sample
+
+## Description
+
+* Simple Ruby on Rails Application
+* Upload and Download CSV file
+* RSpec
+
+
+
+## License
+
+[MIT License](http://www.opensource.org/licenses/MIT).
---
+++ db/seeds.rb
@@ -0,0 +1,31 @@
+# This file should contain all the record creation needed to seed the database with its default values.
+# The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
+#
+# Examples:
+#
+#   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
+#   Mayor.create(name: 'Emanuel', city: cities.first)
+
+Player.create(no: 35, position: 'G', name: '伊藤 優人', born: '1991-08-08')
+Player.create(no: 55, position: 'G', name: '畑 享和', born: '1990-03-17')
+Player.create(no: 5, position: 'D', name: '今城 和智', born: '1987-03-11')
+Player.create(no: 6, position: 'D', name: '山田 佑哉', born: '1984-07-24')
+Player.create(no: 8, position: 'D', name: '菊池 秀治', born: '1986-09-07')
+Player.create(no: 24, position: 'D', name: '佐藤 光', born: '1991-12-03')
+Player.create(no: 54, position: 'D', name: '熊谷 豪士', born: '1988-08-19')
+Player.create(no: 58, position: 'D', name: '篠原 優平', born: '1989-12-25')
+Player.create(no: 58, position: 'D', name: '篠原 優平', born: '1986-02-19')
+Player.create(no: 84, position: 'D', name: '菊池 恭平', born: '1984-04-13')
+Player.create(no: 9, position: 'F', name: '元野 道隆', born: '1987-06-21')
+Player.create(no: 14, position: 'F', name: '田中豪', born: '1983-10-06')
+Player.create(no: 16, position: 'F', name: '岩槻 翔悟', born: '1992-01-10')
+Player.create(no: 17, position: 'F', name: '大宮 良', born: '1992-01-08')
+Player.create(no: 18, position: 'F', name: '水内 直人', born: '1989-08-30')
+Player.create(no: 18, position: 'F', name: '水内 直人', born: '1978-03-10')
+Player.create(no: 21, position: 'F', name: '山本 和輝', born: '1986-11-12')
+Player.create(no: 22, position: 'F', name: '田中 遼', born: '1987-04-22')
+Player.create(no: 28, position: 'F', name: '山下 拓郎', born: '1988-05-22')
+Player.create(no: 33, position: 'F', name: '池田 昇平', born: '1990-07-21')
+Player.create(no: 38, position: 'F', name: '山田 淳哉', born: '1991-11-07')
+Player.create(no: 40, position: 'F', name: '河合 卓真', born: '1988-08-10')
+Player.create(no: 47, position: 'F', name: '篠原 亨太', born: '1984-03-28')
