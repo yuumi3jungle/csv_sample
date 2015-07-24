61f978b74963a3e503d1ed533e88158354ee4127
Yuumi Yoshida <yy@ey-office.com>
2015-07-24 11:43:16 +0900

    Japanize and add validation

--- app/models/player.rb
+++ app/models/player.rb
@@ -1,2 +1,6 @@
 class Player < ActiveRecord::Base
+  validates_presence_of :name
+  validates_numericality_of :no, allow_blank: true
+  validates_inclusion_of :position, in: %w(G D F), allow_blank: true
+  validates_format_of :born, with: /\A\d{4}-\d{1,2}-\d{1,2}\z/, allow_blank: true
 end
