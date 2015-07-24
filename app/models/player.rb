0f3322a6212b04f006c7422380e65baacdcad766
Yuumi Yoshida <yy@ey-office.com>
2015-07-24 18:25:27 +0900

    Add CSV import and merge methds to model

--- app/models/player.rb
+++ app/models/player.rb
@@ -19,4 +19,46 @@ class Player < ActiveRecord::Base
     csv.gsub("\n", "\r\n").encode("Shift_JIS")
   end
 
+  def self.import(raw_csv_string, skip_first_line = true)
+    csv_string = raw_csv_string.force_encoding("Shift_JIS").encode("UTF-8")
+    csv_array = CSV.parse(csv_string)
+
+    players = []
+    errors = []
+    line = 1
+    if skip_first_line
+      csv_array.shift()
+      line = 2
+    end
+
+    csv_array.each do |csv|
+      player = Player.new(Hash[CSV_COLUMNS.zip(csv)])
+      if player.valid?
+        players << player
+      else
+        errors << "#{line}行目: #{player.errors.full_messages.join(', ')}"
+      end
+      line += 1
+    end
+
+    [players, errors]
+  end
+
+  def self.merge(players)
+    errors = []
+
+    players.each do |player|
+      if player.id
+        p = Player.find_by(id: player.id)
+        if p
+          p.update(Hash[CSV_COLUMNS.zip(player.attributes.values_at(*CSV_COLUMNS))])
+        else
+          errors << "id = #{player.id} データは存在しません"
+        end
+      else
+        player.save
+      end
+    end
+    errors
+  end
 end
--- spec/models/player_spec.rb
+++ spec/models/player_spec.rb
@@ -17,8 +17,50 @@ ID,名前,守備,背番号,生年月日\r\n\
     end
   end
 
-  describe "self.mege" do
+  describe "self.import" do
+    it "csv文字列を受け取り、そのデータでPlayerインスタンスの配列を作る" do
+      csv = "ID,名前,守備,背番号,生年月日\r\n1,畑 享和,G,55,1990-03-17\r\n,今城 和智,D,5,1987-03-11\r\n".encode("Shift_JIS")
+      players, errors = Player.import(csv)
+
+      expect(errors.any?).to be_falsy
+      expect(players.size).to eq 2
+      expect(players[0].attributes.values_at(*Player::CSV_COLUMNS)).to eq(
+        [1, "畑 享和", "G", 55, "1990-03-17".to_date])
+      expect(players[1].attributes.values_at(*Player::CSV_COLUMNS)).to eq(
+        [nil, "今城 和智", "D", 5, "1987-03-11".to_date])
+    end
+    it "csv文字列に間違いがあればerrorsを戻す" do
+      csv = "ID,名前,守備,背番号,生年月日\r\n1,畑 享和,X,5a,1990-03-17\r\n2,,D,5,1987-03-11\r\n".encode("Shift_JIS")
+      players, errors = Player.import(csv)
+      expect(errors).to eq ["2行目: 背番号は数値で入力してください, 守備は一覧にありません", "3行目: 名前を入力してください"]
+    end
   end
+
+  describe "self.merge" do
+    it "IDの無いデータはデーブルに追加される" do
+      errors = Player.merge([Player.new(no: 47, position: 'F', name: '篠原 亨太',   born: '1984-03-28')])
+
+      expect(errors.any?).to be_falsy
+      expect(Player.last.name).to eq "篠原 亨太"
+    end
+    it "IDのあるデータはデーブルの旧データを更新する" do
+      player = Player.first
+      player.no = 56
+      errors = Player.merge([player])
+
+      expect(errors.any?).to be_falsy
+      expect(Player.first.no).to eq 56
+    end
+    it "IDはあるデータに対しデーブルに旧データが無ければエラー" do
+      player = Player.last
+      player.id = player.id + 1
+      errors = Player.merge([player])
+
+      expect(errors.count).to eq 1
+      expect(errors[0]).to match /データは存在しません/
+    end
+  end
+
 end
 
 
