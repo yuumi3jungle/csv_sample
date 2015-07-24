0880bd5fce82bb622a8a4195aec143929655d6cd
Yuumi Yoshida <yy@ey-office.com>
2015-07-24 15:02:01 +0900

    Add CSV output method to model

--- app/models/player.rb
+++ app/models/player.rb
@@ -1,6 +1,22 @@
+require 'csv'
+
 class Player < ActiveRecord::Base
   validates_presence_of :name
   validates_numericality_of :no, allow_blank: true
   validates_inclusion_of :position, in: %w(G D F), allow_blank: true
   validates_format_of :born, with: /\A\d{4}-\d{1,2}-\d{1,2}\z/, allow_blank: true
+
+  CSV_COLUMNS = %w(id name position no born)
+
+  def self.csv_all
+    csv = CSV_COLUMNS.map{|c| I18n.t "activerecord.attributes.player.#{c}"}.to_csv
+
+    self.order(:id).each do |e|
+      values = e.attributes.values_at(*CSV_COLUMNS)
+      csv << values.to_csv
+    end
+
+    csv.gsub("\n", "\r\n").encode("Shift_JIS")
+  end
+
 end
--- spec/models/player_spec.rb
+++ spec/models/player_spec.rb
@@ -1,5 +1,24 @@
 require 'rails_helper'
 
 RSpec.describe Player, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  before do
+    create(:player, no: 55, position: 'G', name: '畑 享和',   born: '1990-03-17')
+    create(:player, no: 5,  position: 'D', name: '今城 和智', born: '1987-03-11')
+    create(:player, no: 14, position: 'F', name: '田中豪',    born: '1983-10-06')
+  end
+
+  describe "self.csv_all" do
+    it "全データのCSVが戻る" do
+      expect(Player.csv_all).to eq "\
+ID,名前,守備,背番号,生年月日\r\n\
+1,畑 享和,G,55,1990-03-17\r\n\
+2,今城 和智,D,5,1987-03-11\r\n\
+3,田中豪,F,14,1983-10-06\r\n".encode("Shift_JIS")
+    end
+  end
+
+  describe "self.mege" do
+  end
 end
+
+
